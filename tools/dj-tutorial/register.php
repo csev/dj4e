71b6c00717525ca36598bce36aa0f66e1e74c034
Charles Severance <csev@umich.edu>
2018-08-28 07:37:47 -0400

    Move the folder name

--- tools/dj-tutorial/register.php
+++ tools/dj-tutorial/register.php
@@ -1,7 +1,7 @@
 <?php
 
 $REGISTER_LTI2 = array(
-"name" => "Django Autograder",
+"name" => "Django Tutorial Autograder",
 "FontAwesome" => "fa-code",
 "short_name" => "Django Autograder",
 "description" => "This is an autograder for Django assignments from www.dj4e.com.",
