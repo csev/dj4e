06991d8ac2901939c0484b9a37aee45c649b81f1
Charles Severance <csev@umich.edu>
2019-01-03 11:35:03 -0500

    Get started on admin

--- assn/index.php
+++ assn/index.php
@@ -76,6 +76,7 @@ function onSelect() {
   <option <?= x_sel("paw_github.md") ?>>Using GitHub</option>
   <option <?= x_sel("paw_skeleton.md") ?>>Skeleton web site</option>
   <option <?= x_sel("paw_models.md") ?>>DJAngo Models</option>
+  <option <?= x_sel("paw_admin.md") ?>>DJAngo Admin</option>
 </select>
 </div>
 <?php
@@ -92,6 +93,7 @@ web site.
 <li><a href="paw_github.md">Using GitHub</a></li>
 <li><a href="paw_skeleton.md">Skeleton web site</a></li>
 <li><a href="paw_models.md">DJango Models</a></li>
+<li><a href="paw_admin.md">DJango Admin</a></li>
 </ul>
 <p>
 If you find a mistake in these pages, feel free to send me a fix using
