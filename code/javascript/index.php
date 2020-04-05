989cceb4eb00d0f9a8426b56afa8040187dcb583
Charles Severance <csev@umich.edu>
2020-04-05 10:13:56 -0400

    Progress.

--- code/javascript/index.php
+++ code/javascript/index.php
@@ -11,10 +11,14 @@
 <li><a href="js-09.htm">js-09.htm</a> - functions</li>
 <li><a href="js-10.htm">js-10.htm</a> - global scope</li>
 <li><a href="js-11.htm">js-11.htm</a> - var - local scope</li>
-<li><a href="js-12.htm">js-12.htm</a> - object "associative array"</li>
-<li><a href="js-13.htm">js-13.htm</a> - Javascript error</a></li>
-<li><a href="js-14.htm">js-14.htm</a> - Counted loop</li>
-<li><a href="js-15.htm">js-15.htm</a> - Loop and break</li>
-<li><a href="js-16.htm">js-16.htm</a> - Loop and continue</li>
-<li><a href="js-17.htm">js-17.htm</a> - Try / catch / finally </li>
+
+<li><a href="js-12.htm">js-12.htm</a> - conditional (if) </li>
+<li><a href="js-13.htm">js-13.htm</a> - multi-way if</li>
+<li><a href="js-14.htm">js-14.htm</a> - while loop </li>
+
+<li><a href="js-15.htm">js-15.htm</a> - object "associative array"</li>
+<li><a href="js-16.htm">js-16.htm</a> - Counted loop</li>
+<li><a href="js-17.htm">js-17.htm</a> - Loop and break</li>
+<li><a href="js-18.htm">js-18.htm</a> - Loop and continue</li>
+<li><a href="js-19.htm">js-19.htm</a> - Try / catch / finally </li>
 </ul>
