59298a04956b752bbd5ae2ac58b48a11d19e8ba4
Charles Severance <csev@umich.edu>
2020-04-05 10:05:07 -0400

    Updates

--- code/javascript/index.php
+++ code/javascript/index.php
@@ -1,16 +1,20 @@
+<p>View developer console for these examples</p>
 <ul>
-<li><a href="js-01.htm">js-01.htm - document.write()</a></li>
-<li><a href="js-02.htm">js-02.htm - alert()</a></li>
-<li><a href="js-03.htm">js-03.htm - onclick=</a></li>
-<li><a href="js-04.htm">js-04.htm - src=</a></li>
-<li><a href="js-05.htm">js-05.htm - Syntax errors</a></li>
-<li><a href="js-06.htm">js-06.htm - console.log()</a></li>
-<li><a href="js-07.htm">js-07.htm - white space</a></li>
-<li><a href="js-08.htm">js-08.htm - new lines</a></li>
-<li><a href="js-09.htm">js-09.htm - functions</a></li>
-<li><a href="js-10.htm">js-10.htm - global scope</a></li>
-<li><a href="js-11.htm">js-11.htm - var - local scope</a></li>
-<li><a href="js-12.htm">js-12.htm - object "associative array"</a></li>
-<li><a href="js-13.htm">js-13.htm - Javascript error</a></li>
-<li><a href="js-14.htm">js-14.htm - Try / catch / finally </a></li>
+<li><a href="js-01.htm">js-01.htm</a> - document.write()</li>
+<li><a href="js-02.htm">js-02.htm</a> - alert()</li>
+<li><a href="js-03.htm">js-03.htm</a> - onclick=</li>
+<li><a href="js-04.htm">js-04.htm</a> - src=</li>
+<li><a href="js-05.htm">js-05.htm</a> - Syntax errors</li>
+<li><a href="js-06.htm">js-06.htm</a> - console.log()</li>
+<li><a href="js-07.htm">js-07.htm</a> - white space</li>
+<li><a href="js-08.htm">js-08.htm</a> - new lines</li>
+<li><a href="js-09.htm">js-09.htm</a> - functions</li>
+<li><a href="js-10.htm">js-10.htm</a> - global scope</li>
+<li><a href="js-11.htm">js-11.htm</a> - var - local scope</li>
+<li><a href="js-12.htm">js-12.htm</a> - object "associative array"</li>
+<li><a href="js-13.htm">js-13.htm</a> - Javascript error</a></li>
+<li><a href="js-14.htm">js-14.htm</a> - Counted loop</li>
+<li><a href="js-15.htm">js-15.htm</a> - Loop and break</li>
+<li><a href="js-16.htm">js-16.htm</a> - Loop and continue</li>
+<li><a href="js-17.htm">js-17.htm</a> - Try / catch / finally </li>
 </ul>
---
+++ code/javascript/showsource.js
@@ -0,0 +1,26 @@
+// https://stackoverflow.com/questions/9333914/get-content-inside-script-as-text
+// https://stackoverflow.com/a/34854903
+// https://stackoverflow.com/a/52058753
+
+
+function renderPRE( script, codeScriptName ){
+  var isIE = !document.currentScript;
+  if (isIE) return;
+
+  var jsCode = script.innerHTML.trim();
+  // escape angled brackets between two _ESCAPE_START_ and _ESCAPE_END_ comments
+  let textsToEscape = jsCode.match(new RegExp("// _ESCAPE_START_([^]*?)// _ESCAPE_END_", 'mg'));
+  if (textsToEscape) {
+    textsToEscape.forEach(textToEscape => {
+      jsCode = jsCode.replace(textToEscape, textToEscape.replace(/</g, "&lt")
+        .replace(/>/g, "&gt")
+        .replace("// _ESCAPE_START_", "")
+        .replace("// _ESCAPE_END_", "")
+        .trim());
+    });
+  }
+  script.insertAdjacentHTML('afterend', "<pre class='language-js'><code>" + jsCode + "</code></pre>");
+}
+
+renderPRE(document.getElementsByTagName("script")[0]);
+
