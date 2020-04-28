28b1c73119f15ff02e142f0c65cd4bc5abb770cd
Charles Severance <csev@umich.edu>
2020-04-28 09:42:33 -0400

    Updates

--- tools/crud/webauto.php
+++ tools/crud/webauto.php
@@ -112,7 +112,7 @@ if ( $title ) {
 </p>
 <p>If you need to run this grading program on an application that is running on your
 laptop or desktop computer with a URL like <strong>http://localhost...</strong> you
-will need to install and use the <a href="http://www.dj4e.com/md/" target="_blank">NGrok or LocalTunnel</a>
+will need to install and use the <a href="http://www.dj4e.com/md/" target="_blank">NGrok</a>
 application to get a temporary Internet-accessible URL that can be used with this application.  Make sure to use
 Django's port 8000 and not port 8888 as is used in the above documentation.
 </p>
