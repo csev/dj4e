7dccf76570d2f45e9c35d9ebb79a6d7dddf90724
csev <csev@umich.edu>
2020-09-28 21:40:59 +0000

    Make the check smarter

--- tools/crud/meta_check.php
+++ tools/crud/meta_check.php
@@ -3,8 +3,11 @@
 $meta_good = true;
 line_out("Checking meta tag...");
 $dj4e_meta = webauto_get_meta($crawler, "dj4e");
-if ( strlen($dj4e_meta) < 1 || $dj4e_meta != $check ) {
+if ( strlen($dj4e_meta) < 1 ) {
     error_out('You seem to be missing the required meta name="dj4e" tag. See above.');
+    $meta_good = false;
+} else if ( $dj4e_meta != $check ) {
+    error_out('Your meta name="dj4e" tag of "'.$dj4e_meta.'" is not correct. See above.');
     $meta_good = false;
 }
 
@@ -12,10 +15,25 @@ if ( strlen($dj4e_meta) < 1 || $dj4e_meta != $check ) {
 check_code_and_version($crawler);
 
 // Validate the $crawler code
+// "42"+((Math.floor(d.getTime()/1234567)*123456)+42)
 $dj4e_code = webauto_get_meta($crawler, 'dj4e-code');
-if ( strpos($dj4e_code, "4215") !== 0 ) {
+
+if ( ! $dj4e_code || strlen($dj4e_code) < 1 ) {
     error_out('You seem to be missing the required meta name="dj4e-code" tag.  Check the assignment document.');
-    // TODO: Uncomment this after October 2019
-    // $meta_good = false;
+    $meta_good = false;
+}  else if ( strpos($dj4e_code, "42") !== 0 || strlen($dj4e_code) < 14 ) {
+    error_out('Your meta name="dj4e-code" tag is not formatted correctly.  Check the assignment document.');
+    $meta_good = false;
+} else {
+	$then = substr($dj4e_code,2);
+	$now = time();
+    if ( ($then + 180*24*60*60) < $now ) {
+		error_log('Expired dj4e-code '.$dj4e_code.' user:'.$USER->id.' '.$USER->email." ".$USER->displayname);
+    	error_out('Your meta name="dj4e-code" tag is out of date.  Check the assignment document.');
+    	$meta_good = false;
+	}
 }
 
+// TODO: Check veriosn too :)
+$dj4e_version = webauto_get_meta($crawler, 'dj4e-version');
+error_log('dj4e_version '.$dj4e_version);
