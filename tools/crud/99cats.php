85318654491088d60b920c18416c019b31f95937
Charles Severance <csev@umich.edu>
2020-10-15 22:01:55 -0400

    Update to assignment

--- tools/crud/99cats.php
+++ tools/crud/99cats.php
@@ -3,7 +3,8 @@
 require_once "99fields.php";
 
 $SPEC = $CRUD_FIELDS->cats;
-$SPEC->assignment_type_lower = 'sample exam';
+$SPEC->assignment_type_lower = 'assignment';
+// $SPEC->assignment_type_lower = 'sample exam';
 patchSpec($SPEC);
 
 if ( !isset($SPEC_ONLY) ) require_once("99crud.php");
