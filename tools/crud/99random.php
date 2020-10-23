42976201567543dafc7e8323ea69b5b238251bae
Charles Severance <csev@umich.edu>
2020-10-23 09:44:51 -0400

    Make the random exam online

--- tools/crud/99random.php
+++ tools/crud/99random.php
@@ -14,6 +14,7 @@ $assn = $keys[$pos];
 
 $SPEC = $CRUD_FIELDS->{$assn};
 $SPEC->assignment_type_lower = 'exam';
+$SPEC->online = true;
 patchSpec($SPEC);
 
 if ( !isset($SPEC_ONLY) ) require_once("99crud.php");
