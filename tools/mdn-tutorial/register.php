fd47daef6db3059eb4e9a069e8697894a5b9a24a
Charles Severance <csev@umich.edu>
2022-03-31 18:56:57 -0400

    Tweaks.

--- tools/mdn-tutorial/register.php
+++ tools/mdn-tutorial/register.php
@@ -1,10 +1,10 @@
 <?php
 
 $REGISTER_LTI2 = array(
-"name" => "Mozilla Develoment Network(MDN) Tutorial Autograder",
+"name" => "MDN Tutorial Autograder",
 "FontAwesome" => "fa-code",
 "short_name" => "MDN Autograder",
-"description" => "This is an autograder for Django assignments from www.dj4e.com.",
+"description" => "This is an autograder for MDN assignments from www.dj4e.com.",
 "messages" => array("launch", "launch_grade")
 );
 
