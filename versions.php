a06cd6adede4a6b607437364dd57a5baa3e2e514
Charles Severance <csev@umich.edu>
2023-12-08 17:24:37 -0600

    Updates.

---
+++ versions.php
@@ -0,0 +1,44 @@
+<?php include("top.php");?>
+<?php include("nav.php");?>
+<h1>Course Versions</h1>
+<p>As time passes and Django releases new vesions, this course
+needs to be updated.  Very little changes from one version of Django
+to the next so if this course is not using the *latest* version of
+Django it is usually no problem.  You may see demonstration videos
+with older versions of Django that work with a later version of Django.
+</p>
+<p>
+<a href="https://docs.djangoproject.com/en/5.0/releases/" targwt="_blank">
+Django Releases
+</a></p>
+<h2>Current Version of this Course</h2>
+<pre>
+September 2023
+
+Django 4.2
+Bootstrap 5.0
+jQuery removed
+</pre>
+<h2>Previous Versions of this Course</h2>
+<pre>
+September 2022
+
+Django 4.0
+Bootstrap 3.4.1
+jQuery 3.3.1
+
+
+September 2021
+
+Django 3.2
+Bootstrap 3.4.0
+jQuery 3.3.1
+
+January 2020
+
+Django 3.0.1
+
+</pre>
+
+<?php include("footer.php"); ?>
+
