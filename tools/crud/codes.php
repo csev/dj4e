d05480a73c904c707ba0154b1aad6c70365f760e
Charles Severance <csev@umich.edu>
2024-04-22 09:42:01 -0400

    Keep codes from running out of memory

--- tools/crud/codes.php
+++ tools/crud/codes.php
@@ -25,7 +25,6 @@ WHERE grade = 1 AND link_id = :LNK ORDER BY R.updated_at desc limit 200;";
 
 $sql = "SELECT R.updated_at, JSON_EXTRACT(R.json, '$.dj4e_codes') AS codes, 
 	R.json AS json,
-JSON_EXTRACT(R.json, '$.output') AS output,
 JSON_EXTRACT(R.json, '$.url') AS url,
 JSON_EXTRACT(R.json, '$.dj4e_versions') AS versions, email, displayname 
 FROM lti_result AS R
