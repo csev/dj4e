3e9ce16ca0d09af697cb35923b40e717fb2b02fd
Charles R. Severance <Charles R. Severance>
2025-02-24 20:07:22 +0000

    Better error checking

--- tools/jsauto/mini_01.php
+++ tools/jsauto/mini_01.php
@@ -17,14 +17,25 @@ $penalty = $dueDate->penalty;
 
 $json_params = file_get_contents("php://input");
 if (strlen($json_params) > 0 && isValidJSON($json_params)) {
-    $decoded_input = json_decode($json_params);
-    error_log("Incoming JSON: ".json_encode($decoded_input));
-    $step = $decoded_input->step->step;
-    $response = $decoded_input->response;
+    try {
+        $decoded_input = json_decode($json_params);
+        error_log("===== Incoming JSON: ".json_encode($decoded_input));
+        if ( is_object($decoded_input) && is_object($decoded_input->step) ) {
+            $step = $decoded_input->step->step ?? 0;
+    } else {
+        $step = 0;
+    }
+        $response = $decoded_input->response;
+    } catch (Exception $e) {
+    error_log("===== Unexpected data in JSON, resetting step to 0");
+    $step = 0;
+    }
 } else {
     $step = 0;
 }
 
+$previousgrade = $RESULT->grade;
+error_log("==== Previous grade ".$previousgrade);
 $currentGrade = U::get($_SESSION, "currentgrade", 0.0);
 $oldGrade = $currentGrade;
 
@@ -40,7 +51,6 @@ if ( $step == $checkstep++ ) {
     $nextstep = '{"command": "ping", "text": "42", "message": "Check for correct page load"}';
 
 } else if ( $step == $checkstep++ ) {
-    $text = $response->text;
     $nextstep = '{"command": "switchurl", "text": "/missing", "message": "Switch to /missing url"}';
 
 } else if ( $step == $checkstep++ ) {
@@ -77,10 +87,16 @@ $nextstep = json_decode($nextstep, true);
 $retval = array_merge($retval, $nextstep);
 
 $sendGrade = webauto_compute_effective_score(100.0, $currentGrade, $penalty);
-$scoredetail = webauto_send_score($sendGrade);
-error_log("sendGrade=".$sendGrade." detail=".$scoredetail);
-$retval['detail'] = $scoredetail == true ? "Score sent ".($sendGrade*100) : htmlentities($scoredetail);
+error_log("====== prev $previousgrade / send $sendGrade");
+if ( $sendGrade*100.0 >= $previousgrade ) {
+    $scoredetail = webauto_send_score($sendGrade);
+    error_log("===== sendGrade=".$sendGrade." detail=".$scoredetail);
+    $retval['detail'] = $scoredetail == true ? "Score sent ".($sendGrade*100) : htmlentities($scoredetail);
+} else {
+    $retval['detail'] = "Previous score of ".$previousgrade." higher than current grade ".($sendGrade*100);
+}
 
 $retval = json_encode($retval);
-error_log("Next step: ".$retval);
+error_log("===== Next step: ".$retval);
 echo($retval);
+
