f98d5f78f437f68b1a41e8fce21948199a82eb7a
Charles Severance <csev@umich.edu>
2025-05-25 12:32:09 -0400

    Switch the all important icon...

--- tools/market/register.php
+++ tools/market/register.php
@@ -2,7 +2,7 @@
 
 $REGISTER_LTI2 = array(
 "name" => "DJ4E MarketPlace",
-"FontAwesome" => "fa-bag-shopping",
+"FontAwesome" => "fa-store",
 "short_name" => "DJ4E Market",
 "description" => "This is an autograder for the Marketplace series of Django assignments from www.dj4e.com.",
 "messages" => array("launch", "launch_grade")
