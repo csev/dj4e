c80671735c3699035829934ddb0ce9335f6c23ac
Charles Severance <csev@umich.edu>
2026-02-03 05:15:33 -0500

    Move loading of wc and worker into foter.

--- footer.php
+++ footer.php
@@ -1,15 +1,12 @@
 <?php
 
-// Read service worker registration template
-$swTemplate = file_get_contents(__DIR__ . '/tsugi/lib/src/Controllers/static/ServiceWorker/service-worker-register.php');
-// Extract content after the PHP closing tag
-$swScript = substr($swTemplate, strpos($swTemplate, '?>') + 2);
+// Service worker registration and notifications web component are now loaded
+// in Output.php footerStart() method
 
 $foot = '
 <p style="font-size: 75%; margin-top: 5em;">
 Copyright Creative Commons Attribution 3.0 - Charles R. Severance
-</p><script type="module" src="' . htmlspecialchars(\Tsugi\Controllers\StaticFiles::url('Notifications', 'tsugi-notifications.js')) .'"></script>
-' . $swScript;
+</p>';
 
 $OUTPUT->setAppFooter($foot);
 
